73778ae754f10cbf7954a0b0312836b725e46b79
Neezar <rforlyffe@gmaicl.com>
2022-04-30 23:24:08 +0800

    refactor

--- packages/root-app/src/app/pages/Home/Home.page.tsx
+++ packages/root-app/src/app/pages/Home/Home.page.tsx
@@ -1,16 +1,8 @@
-import {
-  AppShell,
-  Aside,
-  Burger,
-  Footer,
-  Header,
-  MediaQuery,
-  Navbar,
-  Text,
-  useMantineTheme,
-} from "@mantine/core";
-import { motion } from "framer-motion";
+import { AppShell, useMantineTheme } from "@mantine/core";
 import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 const Home = () => {
   const theme = useMantineTheme();
@@ -23,47 +15,10 @@ const Home = () => {
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
       fixed
-      navbar={
-        <Navbar
-          p="sm"
-          hiddenBreakpoint="sm"
-          hidden={!opened}
-          width={{ sm: 200, lg: 300 }}
-        >
-          <motion.button
-            className="p-3 mb-2 bg-red-500 rounded-md"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.2, duration: 0.8 }}
-            whileHover={{ scale: 1.1 }}
-          >
-            Hello
-          </motion.button>
-
-          <Text>Application navbar</Text>
-        </Navbar>
-      }
-      header={
-        <Header height={55} p="md">
-          <div
-            style={{ display: "flex", alignItems: "center", height: "100%" }}
-          >
-            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="md"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
-
-            <Text>Application header</Text>
-          </div>
-        </Header>
-      }
+      navbar={<Navbar opened={opened} />}
+      header={<Header opened={opened} setOpened={setOpened} />}
     >
-      <Text>Resize app to see responsive navbar in action</Text>
+      <Outlet />
     </AppShell>
   );
 };
--- packages/root-app/src/app/store/useSocket.store.ts
+++ packages/root-app/src/app/store/useSocket.store.ts
@@ -1,8 +1,5 @@
-import { toast } from "react-toastify";
 import { Socket } from "socket.io-client";
 import create from "zustand";
-import { useHistory } from "../../bootstrap";
-import agent from "../api/agent";
 import { combineAndImmer } from "./types/combine-Immer";
 
 export const useSocketStore = create(
